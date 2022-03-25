c00b5e76c262960c80283de78962c10b3dd00356
lei <lei006@qq.com>
2022-03-25 18:18:34 +0800

    00

---
+++ src/TinySelect.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="hello">
+    TinyTitle 组件
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TinyTitle',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
