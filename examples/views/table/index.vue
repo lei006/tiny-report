7b0a84585a31fac77c0c60cd0ecbcb4236f2fa50
lei006 <lei006@qq.com>
2022-03-26 20:42:52 +0800

    把  example 改为 vue-admin-template

--- examples/views/table/index.vue
+++ examples/views/table/index.vue
@@ -44,6 +44,7 @@
 </template>
 
 <script>
+import { getList } from '@/api/table'
 
 export default {
   filters: {
@@ -68,6 +69,10 @@ export default {
   methods: {
     fetchData() {
       this.listLoading = true
+      getList().then(response => {
+        this.list = response.data.items
+        this.listLoading = false
+      })
     }
   }
 }
