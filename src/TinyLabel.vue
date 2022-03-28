86b810f4b493f2887f00149ada7eeae98b4c643b
lei <lei006@qq.com>
2022-03-28 16:45:14 +0800

    初步增加了几个控件

---
+++ src/TinyLabel.vue
@@ -0,0 +1,45 @@
+<template>
+    <report-base-item v-model="reportItem">
+      <div class="tiny-report-label">1331</div>
+    </report-base-item>
+</template>
+
+<script>
+
+import ReportBaseItem from './ReportItem/index.vue'
+
+export default {
+  name: 'TinyReportLabel',
+  components:{ReportBaseItem},
+  model: {
+      prop: "reportItem",
+      event: "eventReportItem"
+  },
+  props: {
+      reportItem: {
+          type: Object
+      },
+  },
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+.tiny-report-label{
+  width: 100%;
+  height: 100%;
+  margin: 0px;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+
+</style>
