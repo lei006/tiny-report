b736091d7c179c4a26d8fc0675f87b7a33de4ed5
lei006 <lei006@qq.com>
2022-05-24 23:58:57 +0800

    0

---
+++ src/ReportComponents/TinyTop.vue
@@ -0,0 +1,55 @@
+<template>
+      <div class="tiny-report-item-top-label">
+        <slot>标签</slot>
+      </div>
+</template>
+
+<script>
+
+import ReportBaseItem from './index.vue'
+
+export default {
+  name: 'TinyReportLabel',
+  components:{ReportBaseItem},
+  model: {
+      prop: "reportItem",
+      event: "eventReportItem"
+  },
+  props: {
+      reportItem: {
+          type: Object
+      },
+  },
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+.tiny-report-label{
+  width: 100%;
+  height: 100%;
+  margin: 0px;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.tiny-report-item-top-label {
+
+  position: absolute;
+  border-style:dashed;
+  border-width:0px;
+  top:2px;
+  left:2px;
+}
+
+
+
+</style>
